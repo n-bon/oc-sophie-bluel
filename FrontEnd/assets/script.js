ded2fec8a3a9b32c04b23bc83e8438d7f0d97779
Nathan Bonin <n.bonin@gmx.fr>
2025-04-06 17:04:59 +0200

    Style (Accueil) Ajout de commentaires, meilleure lisibilité

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,7 +1,12 @@
+
+/***************** Import des modules ********************************/
+//Programmes spécifiques à la section portfolio
 import { afficherTravaux } from './portfolio.js';
 
-//Appeler et convertir la liste des travaux
+/******************** Chargement des ressources **********************/
+//Charger et convertir la liste des travaux
 const travaux = await fetch("http://localhost:5678/api/works").then(travaux => travaux.json());
 
-//Appeler la fonction d'affichage des travaux grâce à la liste précédemment créée
+/********************** Affichage dynamique **************************/
+//Appeler la fonction d'affichage des travaux avec la liste précédemment créée
 afficherTravaux(travaux);
