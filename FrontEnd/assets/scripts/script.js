bcbd5c33facb3fbc0dd3e38ee83b6c8ce0c1b37c
Nathan Bonin <n.bonin@gmx.fr>
2025-04-07 14:27:25 +0200

    Build (script.js) Ajout de l'écouteur de clic sur les boutons filtres

--- FrontEnd/assets/scripts/script.js
+++ FrontEnd/assets/scripts/script.js
@@ -10,9 +10,48 @@ const travaux = await fetch("http://localhost:5678/api/works").then(travaux => t
 //Charger et convertir la liste des catégories
 const categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json());
 
-/********************** Affichage dynamique **************************/
+/********************** Affichage dynamique du portfolio **************************/
 //Appeler la fonction d'affichage des travaux avec la liste précédemment créée
 afficherTravaux(travaux);
 
 //Appeler la fonction d'affichage dynamique des boutons
 afficherBoutonsFiltres(categories);
+
+/********** Modification dynamique du portfolio avec les filtres ********************/
+
+//Déselectionner les autres filtres
+async function effacerSelectionFiltres (listeBoutons) {
+    listeBoutons.forEach((bouton) => {
+        if (bouton.className === "filtreActif") {
+            bouton.className = "";
+        };
+    });
+}
+
+
+
+async function modifierPortfolioFiltres (tousTravaux) {
+    /******************Initialisation*********/
+    // Selection des objets HTML
+    const sectionBoutonsFiltres = document.querySelector(".filtres");
+    const boutons = sectionBoutonsFiltres.querySelectorAll("button");
+
+    //Création de la variable utilisée pour le filtrage 
+    let listeFiltree = [];
+
+    //Écouter l'événement clic
+    boutons.forEach((bouton) => {
+        bouton.addEventListener("click", (clic) => {
+            //Effacer classe selectionnee sur les autres boutons
+            effacerSelectionFiltres(boutons);
+            //Ajouter la classe sur le bouton
+            bouton.className= "filtreActif";
+            //Creation de la nouvelle liste filtrée
+
+            //Appel de la fonction Afficher Travaux avec la nouvelle liste
+        });
+    });
+
+};
+
+modifierPortfolioFiltres(travaux);
